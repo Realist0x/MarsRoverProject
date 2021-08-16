0be3342c76d696aaa6b225cd9be1b38046c19363
evrimmutlu <evrimtugay2@gmail.com>
2021-08-16 15:31:57 +0300

    Add project files.

---
+++ MarsRover.App/Extensions/CommonExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.App.Extensions
+{
+    public static class CommonExtensions
+    {
+        public static Exception GetInnerException(Exception ex)
+        {
+            var innerException = ex;
+            while (innerException != null && innerException.InnerException != null)
+            {
+                innerException = innerException.InnerException;
+            }
+            return innerException;
+        }
+    }
+}
